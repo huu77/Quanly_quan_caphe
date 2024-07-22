4ff8e27054348e9e88563d6422264020995d1f9e
leduykhanh1611 <khanhduy4569@gmail.com>
2024-07-22 11:41:44 +0700

    crud profile

--- backend/src/modules/router.js
+++ backend/src/modules/router.js
@@ -4,8 +4,10 @@ const routers = express.Router();
 const StatusRouter = require('./Table_Status/router')
 const RoleRouter = require('./Table_Role/router')
 const AccountRouter = require('./Table_Account/router')
+const ProfileRouter = require('./Table_Profile/router')
 routers.use(StatusRouter)
 routers.use(RoleRouter)
 routers.use(AccountRouter)
+routers.use(ProfileRouter)
 
 module.exports = routers
