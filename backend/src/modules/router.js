56efae1d9392473337ed136e54fdf608fa7f9de2
huu77 <thuu28052002@gmail.com>
2024-07-22 11:52:59 +0700

    fix: add router

--- backend/src/modules/router.js
+++ backend/src/modules/router.js
@@ -4,6 +4,9 @@ const routers = express.Router();
 const StatusRouter = require('./Table_Status/router')
 const RoleRouter = require('./Table_Role/router')
 const TableRouter = require('./Table_Table/router')
+const AccountRouter = require('./Table_Account/router')
+const ProfileRouter = require('./Table_Profile/router')
+
 routers.use(StatusRouter)
 routers.use(RoleRouter)
 routers.use(AccountRouter)
