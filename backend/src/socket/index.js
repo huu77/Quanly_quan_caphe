c21e4c72f00cb3e2a755885ae07f67297711a491
huu77 <thuu28052002@gmail.com>
2024-07-23 22:39:32 +0700

    fix: socket

--- backend/src/socket/index.js
+++ backend/src/socket/index.js
@@ -1,35 +1,19 @@
-const {Server} = require('socket.io');  // Import socket.io
-const { availableParallelism } = require('node:os');
-const cluster = require('node:cluster');
+const { Server } = require('socket.io');
 const { createAdapter, setupPrimary } = require('@socket.io/cluster-adapter');
-require('dotenv').config();
-
-if (cluster.isPrimary) {
-    const numCPUs = availableParallelism();
-    // create one worker per available core
-    for (let i = 0; i < numCPUs; i++) {
-      cluster.fork({
-        PORT: 3000 + i
-      });
-    }
-    
-    // set up the adapter on the primary thread
-    return setupPrimary();
-  }
-module.exports = (server) => {
-    const io = new Server(server, {
-        connectionStateRecovery: {
-          maxDisconnectionDuration: 2 * 60 * 1000, // tối đa 2 phút
-        },
-        adapter: createAdapter()
-      });  
  
+
+module.exports = (server) => {
+  const io = new Server(server, {
+    connectionStateRecovery: {
+      maxDisconnectionDuration: 2 * 60 * 1000, // tối đa 2 phút
+    },
+
+  });
+
   // Listen for socket connections
   io.on('connection', (socket) => {
     console.log('a user connected');
-   //   joinRoom(socket);
-    //   broadcast(io);
-    //   leaveRoom(socket);
+
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
@@ -37,9 +21,3 @@ module.exports = (server) => {
 
   return io;
 };
-// ví du 
-// module.exports = (socket) => {
-//     socket.leave('some room');
-//     console.log('User left some room');
-//   };
-  
