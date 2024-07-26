93d157978fa1d6c45a0d64d3c5227b46970448f5
huu77 <thuu28052002@gmail.com>
2024-07-26 15:12:34 +0700

    fix: by huu

--- Dashboard/src/modules/Menu/Orther/Tab2.jsx
+++ Dashboard/src/modules/Menu/Orther/Tab2.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
-import { useGetsStatusTableQuery, usePostStatusTableMutation } from "../../../apis/slices/Status";
 
+import {
+  useGetsStatusTableQuery,
+  usePostStatusTableMutation,
+} from "../../../apis/slices/Table";
+import { AiOutlineDelete,AiFillEdit  } from "react-icons/ai";
 const Tab2 = () => {
   const [newStatus, setNewStatus] = useState("");
   const { data, error, isLoading, refetch } = useGetsStatusTableQuery();
+
   const [postStatusTable] = usePostStatusTableMutation();
 
   const handleCreateStatus = async () => {
@@ -15,9 +20,13 @@ const Tab2 = () => {
       console.error("Failed to create status:", error.message);
     }
   };
+  const handleShowModal = () => {
+    document.getElementById("modalupdatestatustable").showModal();
+  };
+  [getTextUpdate,setTextUpdate] = useState("")
+const handleUpdate=()=>{
 
- 
-
+}
   return (
     <>
       <div className="flex justify-start items-start gap-10 mb-5">
@@ -31,7 +40,10 @@ const Tab2 = () => {
             onChange={(e) => setNewStatus(e.target.value)}
           />
         </label>
-        <button className="btn btn-outline btn-success" onClick={handleCreateStatus}>
+        <button
+          className="btn btn-outline btn-success"
+          onClick={handleCreateStatus}
+        >
           Tạo trạng thái bàn
         </button>
       </div>
@@ -42,6 +54,8 @@ const Tab2 = () => {
             <tr>
               <th>#</th>
               <th>Name</th>
+              <th>Chỉnh sửa</th>
+              <th>Xóa</th>
             </tr>
           </thead>
           <tbody>
@@ -49,11 +63,36 @@ const Tab2 = () => {
               <tr key={status.id}>
                 <th>{status.id}</th>
                 <td>{status.name}</td>
+                <td className="cursor-pointer" onClick={handleShowModal}>
+                  <AiFillEdit  size={24} />
+                </td>
+                <td className="cursor-pointer" onClick={handleShowModal}>
+                  <AiOutlineDelete  size={24} />
+                </td>
               </tr>
-            )) || <tr><td colSpan="2">No data available</td></tr>}
+            )) || (
+              <tr>
+                <td colSpan="2">No data available</td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
+      {/* modal */}
+
+      <dialog id="modalupdatestatustable" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Sửa lại</h3>
+          <input type="text" placeholder=" " value={getTextUpdate} onChange={(e)=>setTextUpdate(e.target.value)} className="input input-bordered w-full max-w-xs mt-5" />
+          <div className="modal-action">
+          <button className="btn btn-outline" onClick={handleUpdate}>Sửa</button>
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </>
   );
 };
--- Dashboard/src/store/middleware.js
+++ Dashboard/src/store/middleware.js
@@ -3,11 +3,11 @@
 import Product from "@apis/slices/Product";
 import Account from "@apis/slices/Account";
 import Table from "@apis/slices/Table";
-import Status from "@apis/slices/Status";
+
 const middleware = (getDefaultMiddleware) =>
   getDefaultMiddleware()
     .concat(Product.middleware)
     .concat(Account.middleware)
-    .concat(Table.middleware)
-    .concat(Status.middleware)
+    .concat(Table.middleware);
+
 export default middleware;
